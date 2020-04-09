6ad8b3a4910e053e4f5c15be25b18d9fa042bcee
Nauraiz Mushtaq <39919890+nauraizmushtaq@users.noreply.github.com>
2020-04-09 19:50:15 +0500

    Add files via upload

---
+++ Nauraiz.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Nauraiz extends Component {
+  state = {};
+  render() {
+    return (
+      <header className="App-header">
+        <h1>M. Nauraiz Mushtaq</h1>
+        <a href="https://www.dappuniversity.com/articles/ethereum-dapp-react-tutorial">
+          How to Link React.js with Ethereum
+        </a>
+      </header>
+    );
+  }
+}
+
+export default Nauraiz;
